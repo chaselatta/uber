7517f00eae85097fdeb3325fa857aca7f012cbd7
Chase Latta <chaselatta@gmail.com>
2025-06-22 15:26:07 -0700

    add color printing

---
+++ cmd/uber/colors.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// ANSI color codes
+const (
+	ColorGreen  = "\033[32m"
+	ColorYellow = "\033[33m"
+	ColorRed    = "\033[31m"
+	ColorReset  = "\033[0m"
+)
+
+// IsTTY checks if stdout is connected to a terminal
+func IsTTY() bool {
+	fileInfo, _ := os.Stdout.Stat()
+	return (fileInfo.Mode() & os.ModeCharDevice) != 0
+}
+
+// IsTTYStderr checks if stderr is connected to a terminal
+func IsTTYStderr() bool {
+	fileInfo, _ := os.Stderr.Stat()
+	return (fileInfo.Mode() & os.ModeCharDevice) != 0
+}
+
+// ColorPrint prints colored text only if running in a TTY
+func ColorPrint(color, message string) {
+	if IsTTY() {
+		fmt.Print(color + message + ColorReset)
+	} else {
+		fmt.Print(message)
+	}
+}
+
+// ColorPrintError prints colored error text only if running in a TTY
+func ColorPrintError(message string) {
+	if IsTTYStderr() {
+		fmt.Fprint(os.Stderr, ColorRed+message+ColorReset)
+	} else {
+		fmt.Fprint(os.Stderr, message)
+	}
+}
